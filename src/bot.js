459537d0fa4c94783ad2924396f3df589f343dfa
Wesley Playz <93501085+Wesley-Playz@users.noreply.github.com>
2025-02-02 20:39:18 -0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -29,6 +29,7 @@ client.on('messageCreate', message => {
     }
 
     const copyPasta = "I'd just like to interject for a moment. What you're referring to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.\nMany computer users run a modified version of the GNU system every day, without realizing it. Through a peculiar turn of events, the version of GNU which is widely used today is often called Linux, and many of its users are not aware that it is basically the GNU system, developed by the GNU Project.\nThere really is a Linux, and these people are using it, but it is just a part of the system they use. Linux is the kernel: the program in the system that allocates the machine's resources to the other programs that you run. The kernel is an essential part of an operating system, but useless by itself; it can only function in the context of a complete operating system. Linux is normally used in combination with the GNU operating system: the whole system is basically GNU with Linux added, or GNU/Linux. All the so-called Linux distributions are really distributions of GNU/Linux!";
+	const tuxCopyPasta = "I love the Linux penguin, Tux. Tux is awesome. You should use Linux.";
 
     let msg = message.content;
 
@@ -41,9 +42,13 @@ client.on('messageCreate', message => {
         return;
     } else if (msg.includes("gnu/linux")) {
         return;
-    } else if (msg.includes("linux") || msg.includes("penguin") || msg.includes("tux")) {
-        msg = removeCharacters(msg, "/");
-        message.reply(copyPasta);
+    } else if (msg.includes("linux")) {
+		msg = removeCharacters(msg, "/");
+		message.reply(copyPasta);
+		return;
+	} else if (msg.includes("tux") || msg.includes("penguin")) {
+        message.reply("https://tenor.com/view/penguin-linux-tux-tux-linux-gif-341950137175367051");
+		message.reply(tuxCopyPasta);
     } else if (msg.includes("loonux")) {
         message.reply(":middle_finger:");
     } else if (msg.includes("mac")) {
